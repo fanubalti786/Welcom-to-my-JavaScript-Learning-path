af51138000630184c1d231f7848e3a675da9e28d
fanubalti786 <fanubalti786@gmail.com>
2024-07-03 06:52:03 +0500

    access api request data througy button in html

--- collectionOfJS/lecture13-Apis.js
+++ collectionOfJS/lecture13-Apis.js
@@ -1,11 +1,48 @@
 const URL = "https://cat-fact.herokuapp.com/facts";
 
 
-let promise = fetch(URL);
-console.log(promise);
+// let promise = fetch(URL);
+// console.log(promise);
 
 
-let fact = document.querySelector(".fact");
+
+// const getfacts = async () => 
+//    {
+//       let response = await fetch(URL);
+//       console.log(response);
+//       console.log(response.status);
+//       let data = await response.json();
+//       console.log(data, data.text);
+         
+//    }
+// // 
+
+// getfacts();
+
+
+
+
+
+// let factpara = document.querySelector(".fact");
+
+// const getfacts = async () => 
+//    {
+//       let response = await fetch(URL);
+//       console.log(response);
+//       console.log(response.status);
+//       let data = await response.json();
+//       console.log(data);
+//       factpara.innerText = data[0].text;
+      
+//    }
+
+
+// getfacts();
+
+
+
+let factpara = document.querySelector(".fact");
+let btn = document.querySelector(".btn");
 
 const getfacts = async () => 
    {
@@ -13,29 +50,17 @@ const getfacts = async () =>
       console.log(response);
       console.log(response.status);
       let data = await response.json();
-      console.log(data.text);
-         console.log(data[0].text);
-   }
-// 
-
-getfacts();
-
-
-
-
-
-let factpara = document.querySelector(".fact");
-
-const getfacts = async () => 
-   {
-      let response = await fetch(URL);
-      // console.log(response);
-      // console.log(response.status);
-      let data = await response.json();
       console.log(data);
-      factpara.innerText = data[0].text;
+    //   factpara.innerText = data[0].text;
+    //   factpara.innerText = data[1].text;
+      factpara.innerText = data[2].text;
+
+
       
    }
 
 
-getfacts();
+btn.addEventListener("click",getfacts);
+
+
+
