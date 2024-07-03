c80a990d49b7708941da99e78ac3d837ce31d6fe
fanubalti786 <fanubalti786@gmail.com>
2024-07-03 07:12:09 +0500

    get api request same procedure according to Promise chain

--- collectionOfJS/lecture13-Apis.js
+++ collectionOfJS/lecture13-Apis.js
@@ -6,45 +6,45 @@ const URL = "https://cat-fact.herokuapp.com/facts";
 
 
 
-// const getfacts = async () => 
-//    {
-//       let response = await fetch(URL);
-//       console.log(response);
-//       console.log(response.status);
-//       let data = await response.json();
-//       console.log(data, data.text);
-         
-//    }
-// // 
-
-// getfacts();
-
-
-
-
-
-// let factpara = document.querySelector(".fact");
-
-// const getfacts = async () => 
-//    {
-//       let response = await fetch(URL);
-//       console.log(response);
-//       console.log(response.status);
-//       let data = await response.json();
-//       console.log(data);
-//       factpara.innerText = data[0].text;
-      
-//    }
-
-
-// getfacts();
-
-
-
-let factpara = document.querySelector(".fact");
-let btn = document.querySelector(".btn");
-
 const getfacts = async () => 
+   {
+      let response = await fetch(URL);
+      console.log(response);
+      console.log(response.status);
+      let data = await response.json();
+      console.log(data, data.text);
+         
+   }
+
+
+getfacts();
+
+
+
+
+
+let factpara1 = document.querySelector(".fact");
+
+const getfacts1 = async () => 
+   {
+      let response = await fetch(URL);
+      console.log(response);
+      console.log(response.status);
+      let data = await response.json();
+      console.log(data);
+      factpara.innerText = data[0].text;
+      
+   }
+
+
+getfacts();
+
+
+
+const factpara2 = document.querySelector(".fact");
+const btn = document.querySelector(".btn");
+
+const getfacts2 = async () => 
    {
       let response = await fetch(URL);
       console.log(response);
@@ -59,8 +59,32 @@ const getfacts = async () =>
       
    }
 
-
+   
 btn.addEventListener("click",getfacts);
+
+
+let getapi = () =>
+    {
+        let data = fetch(URL)
+        .then((response) =>
+        {
+            console.log(response);
+            console.log(response.status);
+            return response.json();
+
+        })
+        .then((data) =>
+        {
+            console.log(data);
+            // factpara.innerText = data[0].text;
+            //factpara.innerText = data[1].text;
+            factpara.innerText = data[2].text;
+        })
+    }
+
+
+btn.addEventListener("click",getapi);
+
 
 
 
