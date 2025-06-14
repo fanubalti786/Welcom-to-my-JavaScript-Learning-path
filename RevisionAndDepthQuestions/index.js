151965783078bc1b8f019fc1d3a057d70b0ace8d
fanubalti786 <fanubalti786@gmail.com>
2025-06-15 04:15:55 +0500

    new additnon or new concept

--- RevisionAndDepthQuestions/index.js
+++ RevisionAndDepthQuestions/index.js
@@ -3,35 +3,19 @@
 
 // Solution:
 let voter;
-let info = [{Name: "irfan", age: 24, gender: "male"}, {Name: "mumtaz", age:15, gender: "male"}, 
-    {Name: "sohail", age:20, gender: "female"}, {Name: "liaqat", age: 32, gender: "male"}
-]
+let info = [
+    { Name: "irfan", age: 24, gender: "male" },
+    { Name: "mumtaz", age: 15, gender: "male" },
+    { Name: "sohail", age: 20, gender: "female" },
+    { Name: "liaqat", age: 32, gender: "male" }
+];
 
 function canvote(info){
-    voter = info.filter(
-        (item) => {
-            return item.age > 18 && item.gender === "male";
-        }
-    )
+    voter = info
+            .filter(item => item.age > 18 && item.gender === "male")
+            .map(item => item.Name).length;
         
     return voter;
 }
 
-console.log(canvote(info));
-
-// let voter;
-// let info = [
-//     { Name: "irfan", age: 24, gender: "male" },
-//     { Name: "mumtaz", age: 15, gender: "male" },
-//     { Name: "sohail", age: 20, gender: "female" },
-//     { Name: "liaqat", age: 32, gender: "male" }
-// ];
-
-// function canvote(info) {
-//     voter = info.filter((item) => {
-//         return item.age > 18 && item.gender === "male";
-//     });
-//     return voter;
-// }
-
-// console.log(canvote(info));
+console.log(canvote(info))
